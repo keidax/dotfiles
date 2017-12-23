b7c5ad9810556e9bf8883e532884e636c6228224
Gabriel Holodak <gthepiper@gmail.com>
2017-12-23 12:33:57 -0500

    Update `internet-info` for Arch Linux

--- bin/internet-info.sh
+++ bin/internet-info.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # Inspired by
 # https://github.com/jldeen/bad-ass-terminal/blob/master/bin/internet_info.sh
 
 if [ "$OS" = "Darwin" ]; then
-    network_name=$(networksetup -getairportnetwork en0 | cut -c 24-)
+    network_name="$(networksetup -getairportnetwork en0 | cut -c 24-)"
     local_ip=$(ipconfig getifaddr en0)
+elif [ "$OS" = "Linux" ]; then
+    if command_exists nmcli; then
+        devname=$(nmcli -t -f type,device dev status | egrep '^wifi' | cut -c 6-)
+        network_name="$(nmcli -t -f ap dev show $devname | grep 'SSID' | cut -c 12-)"
+        local_ip=$(nmcli -t -f ip4 dev show $devname | grep 'ADDRESS' | cut -d: -f2 | cut -d/ -f1)
+    fi
 fi
 
 printf "#[fg=green] %s#[fg=colour15]|#[fg=green]%s" "$network_name" "$local_ip"
