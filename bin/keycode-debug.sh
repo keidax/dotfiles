d9e71021c8dc267b4f3d1f4ab89cd79943aff53a
Gabriel Holodak <gthepiper@gmail.com>
2018-01-14 22:26:31 -0500

    Update `keycode-debug` to be more similar to showkeys

--- bin/keycode-debug.sh
+++ bin/keycode-debug.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Print all keystrokes in hex, to decipher exactly what escape sequences your
-# terminal is sending.
+# terminal is sending. Similar to `showkeys -a`.
 
 # Use /bin/stty to get the native BSD version on Mac
 STTY_PROG="/bin/stty"
@@ -10,7 +10,7 @@ STTY_PROG="/bin/stty"
 tty_state=$("$STTY_PROG" -g)
 finish() {
     "$STTY_PROG" "$tty_state"
-    echo "[TTY restored]"
+    printf '\n[TTY restored]\n'
     exit 0
 }
 trap finish EXIT
@@ -20,14 +20,15 @@ trap ':' INT TERM QUIT
 # Stty settings
 #   raw:    raw input, no delay
 #   opost:  do output processing (for newlines)
-#   -echo:  make sure input is not echoed
+#   echo:   echo input
+#   echoctl:    echo control characters as e.g. '^D'
 #   isig:   enable control characters
 #   susp:   disable
 #   intr:   enable as Ctrl-\
 #   quit:   disable
 # The end result is that all input is sent, except Ctrl-\ is used to quit.
-"$STTY_PROG" raw opost -echo isig susp "undef" intr '^\' quit 'undef'
+"$STTY_PROG" raw opost 'echo' echoctl isig susp 'undef' intr '^\' quit 'undef'
 
 echo "[TTY captured, quit with Ctrl-\\]"
 
-hexdump -v -e '/1 "%4_ad# 0x%x"' -e '/1 " = %3_u\n"'
+hexdump -v -e '"\t" /1 " %3d"' -e '/1 " 0x%02x"' -e '" = %-3_u\n"'
