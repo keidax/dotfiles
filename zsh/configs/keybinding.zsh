7deaee8d637dd579b20a02d2ed125a9e42fe6ddd
Gabriel Holodak <gthepiper@gmail.com>
2018-01-14 19:25:29 -0500

    Zsh: Fix Delete, Home, End keys

--- zsh/configs/keybinding.zsh
+++ zsh/configs/keybinding.zsh
@@ -18,6 +18,25 @@ bindkey '^h' backward-delete-char
 bindkey '^?' backward-delete-char
 bindkey '^w' backward-kill-word
 
+# Terminfo handler
+function bindkey-terminfo {
+    [[ -n "$terminfo[$2]" ]] && bindkey -M "$1" "$terminfo[$2]" "$3"
+}
+
+# 'Delete' key
+bindkey-terminfo viins kdch1 delete-char
+bindkey-terminfo vicmd kdch1 delete-char
+# Shifted 'Delete' key
+bindkey-terminfo viins kDC kill-word
+bindkey-terminfo vicmd kDC kill-word
+# Home key
+bindkey-terminfo viins khome vi-beginning-of-line
+bindkey-terminfo vicmd khome vi-beginning-of-line
+# End key
+bindkey-terminfo viins kend vi-end-of-line
+bindkey-terminfo vicmd kend vi-end-of-line
+
+
 # https://stackoverflow.com/a/35874797
 function Resume {
     jobs %% >/dev/null 2>&1 && {
