553158691eb81297ed2303dca5c69fc95d9bce57
Gabriel Holodak <gthepiper@gmail.com>
2021-12-14 11:11:33 -0500

    Shell: update aliases

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -2,11 +2,11 @@
 source_if_exists "$DOTDIR/shell/aliases"
 
 # Can't assign '=' alias using the 'alias' builtin, but this works
-aliases[=]='_calc'
+aliases[=]='noglob _calc'
 
 # Shorter shell help name
 alias run-help > /dev/null && unalias run-help
 alias help=run-help
 
 # Reverse of cd
-alias pd=popd
+alias pd='popd > /dev/null'
