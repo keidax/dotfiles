41edb6fdad7b8ed9893c58365c199e08d37fa1d2
Gabriel Holodak <gthepiper@gmail.com>
2023-08-22 14:47:05 -0400

    Git: Support diff-so-fancy for interactive staging

--- bin/git-diff-decorate.sh
+++ bin/git-diff-decorate.sh
@@ -2,8 +2,19 @@
 #
 # Wrapper to select best available diff highlighting option
 
+patch=0
+
+if [[ "$1" == "--patch" ]]; then
+    patch=1
+    shift
+fi
+
 if command_exists 'diff-so-fancy'; then
-    exec diff-so-fancy "$@"
+    if [[ $patch -ne 0  ]]; then
+        exec diff-so-fancy --patch "$@"
+    else
+        exec diff-so-fancy "$@"
+    fi
 elif command_exists 'diff-highlight'; then
     exec diff-highlight "$@"
 else
