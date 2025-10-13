2fd7032f474b185ef09380502f6fac27b73cf8c0
Gabriel Holodak <gabriel.holodak@accessibleweb.com>
2025-10-12 23:20:02 -0400

    zsh: set up nodenv

--- zsh/configs/functions.zsh
+++ zsh/configs/functions.zsh
@@ -1,1 +1,6 @@
 # $DOTDIR/shell/functions already loaded by zshenv
+
+if [[ -x ~/.nodenv/bin/nodenv ]]; then
+    # Set up nodenv as a function, along with path modifications
+    eval "$(~/.nodenv/bin/nodenv init - --no-rehash zsh)"
+fi
